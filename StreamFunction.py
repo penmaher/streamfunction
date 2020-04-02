d73665e5cb3abf089b154f7b87c803e51b9d2e92
Penelope Maher <p.maher@exeter.ac.uk>
2020-04-02 10:57:38 +0100

    Python 3 compatable

--- StreamFunction.py
+++ StreamFunction.py
@@ -9,6 +9,7 @@ __author__ = 'Penelope Maher'
 #   Psi = ( (2 pi a cos(lat)) /g) integrate ^P _0 [v] dp 
 #   [v] = (g/(2 pi a cos (lat))) (partial Phi/ partial p) 
 #   [omega] = (-g/(2 pi a^2 cos (lat))) (partial Phi/ partial lat) 
+#   Psi units -> Pa m (s2/m) (m/s) = Pa m/s = kg/(ms2) (m/s) = kg /s 
 
 def cal_stream_fn(data):
   'Calculates the mass stream function. \
@@ -29,25 +30,22 @@ def cal_stream_fn(data):
   omega = data['omega']
   vcomp = data['vcomp']
 
-  assert len(omega.shape)  == 3 , 'Data is not a 3D array. Ensure data is a zonal mean' 
-
   assert isinstance(omega,np.ma.core.MaskedArray)  == True , 'Data is not masked' 
   
-  print '... Integrate first direction'
+  print('... Integrate first direction')
   
-  for t in xrange(len(data['time'])): 
-    for p in xrange(len(data['pfull']) -1): 
-
+  for t in range(len(data['time'])): 
+    for p in range(len(data['pfull']) -1): 
       psi_lat[t,p,0]=0
       psi_lat_rev[t,p,0]=0
 
-      for l in xrange(len( data['lat']) -1):
+      for l in range(len( data['lat']) -1):
         w_val = np.array([omega[t,p,l],omega[t,p+1,l],omega[t,p,l+1],omega[t,p+1,l+1]])
         if np.ma.count(w_val) == 4:
           dlat=lat_rad[l] - lat_rad[l+1]
           psi_lat[t,p,l+1] = psi_lat[t,p,l] + dlat * fn_const_dlat * np.cos(lat_rad[l+1]) * 0.25*(np.sum(w_val))
         else:  
-          print 'Address missing values if required'
+          print('Address missing values if required')
           pdb.set_trace()
 
       for l in range(len(data['lat'])-1,0,-1 ): 
@@ -56,25 +54,24 @@ def cal_stream_fn(data):
           dlat=lat_rad[l] - lat_rad[l-1]   #positive
           psi_lat_rev[t,p,l-1] = psi_lat_rev[t,p,l]+ dlat*fn_const_dlat*np.cos(lat_rad[l-1])* 0.25*(np.sum(w_val_rev))
         else:
-          print 'Address missing values if required'
+          print('Address missing values if required')
           pdb.set_trace()
     
   #integrate  bottom to top and then top to bottom [v] 
-  print '... Integrate second direction'  
-
-  for t in xrange(len(data['time'])): 
-    for l in xrange(len(data['lat']) -1):
+  print('... Integrate second direction')  
+  for t in range(len(data['time'])): 
+    for l in range(len(data['lat']) -1):
       fn_const_dp = (2*np.pi*rad_earth*np.cos(lat_rad[l+1]))/grav
       psi_p[t,0,l] = 0
       psi_p_rev[t,0,l] = 0
       
-      for p in xrange(len(data['pfull'])-1):
+      for p in range(len(data['pfull'])-1):
         v_val=np.array([vcomp[t,p,l],vcomp[t,p,l+1],vcomp[t,p+1,l],vcomp[t,p+1,l+1]])
         if np.ma.count(v_val) == 4:
           dp = (data['pfull'][p]-data['pfull'][p+1])*100   #in Pa and negative
           psi_p[t,p+1,l]=psi_p[t,p,l]+dp * fn_const_dp * 0.25*(np.sum(v_val))
         else: 
-          print 'Address missing values if required'
+          print('Address missing values if required')
           pdb.set_trace()  
   
       for p in range(len(data['pfull'])-1,0,-1 ):
@@ -83,10 +80,10 @@ def cal_stream_fn(data):
           dp=(data['pfull'][p] - data['pfull'][p-1])*100   #in Pa and positive
           psi_p_rev[t,p-1,l] = psi_p_rev[t,p,l] + dp*fn_const_dp* 0.25*(np.sum(v_val_rev))
         else:  
-          print 'Address missing values if required'
+          print('Address missing values if required')
           pdb.set_trace()
     
-  print '  Average the stream functions'       
+  print('  Average the stream functions')       
   psi_final = np.zeros([4,len(data['time']),len(data['pfull']),len(data['lat'])])
   psi_final[0,:,:,:] = psi_lat
   psi_final[1,:,:,:] = psi_p
@@ -96,6 +93,5 @@ def cal_stream_fn(data):
   #take the mean over the four stream funstions
   psi = np.mean(psi_final,axis=0)
   
-  #units -> Pa m (s2/m) (m/s) = Pa m/s = kg/(ms2) (m/s) = kg /s 
 
   return psi
