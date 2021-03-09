66c35102e04036bb57b26596ddc6ae52b8dd4060
Penelope Maher <p.maher@exeter.ac.uk>
2021-03-09 14:40:21 +0000

    Add a common var name for use for pressure dim. Attempting to manage masked values which are giving nans throughout column

--- StreamFunction.py
+++ StreamFunction.py
@@ -11,10 +11,10 @@ __author__ = 'Penelope Maher'
 #   [omega] = (-g/(2 pi a^2 cos (lat))) (partial Phi/ partial lat) 
 #   Psi units -> Pa m (s2/m) (m/s) = Pa m/s = kg/(ms2) (m/s) = kg /s 
 
-def cal_stream_fn(data):
-  'Calculates the mass stream function. \
-  Data is a dictionary containing masked arrays of omega, v wind, lat, time and pressure (hPa).'
-
+def cal_stream_fn(data, pres_var_name):
+  '''Calculates the mass stream function. 
+     Data is a dictionary containing masked zonal-mean arrays of omega and vcomp
+     The dims are named: time, lat, and the input pres_var_name (hPa).'''
   
   grav=9.80665 #m/s
   rad_earth=6378100.0 #m
@@ -23,39 +23,48 @@ def cal_stream_fn(data):
 
   #integrate right to left [omega] and then integrate left to right [omega]
   
-  psi_p = np.zeros((len(data['time']),len(data['pfull']),len(data['lat'])))
-  psi_lat = np.zeros((len(data['time']),len(data['pfull']),len(data['lat'])))
-  psi_p_rev = np.zeros((len(data['time']),len(data['pfull']),len(data['lat'])))
-  psi_lat_rev = np.zeros((len(data['time']),len(data['pfull']),len(data['lat'])))
+  psi_p = np.zeros((len(data['time']),len(data[pres_var_name]),len(data['lat'])))
+  psi_lat = np.zeros((len(data['time']),len(data[pres_var_name]),len(data['lat'])))
+  psi_p_rev = np.zeros((len(data['time']),len(data[pres_var_name]),len(data['lat'])))
+  psi_lat_rev = np.zeros((len(data['time']),len(data[pres_var_name]),len(data['lat'])))
   omega = data['omega']
   vcomp = data['vcomp']
   assert isinstance(omega,np.ma.core.MaskedArray)  == True , 'Data is not masked' 
+  assert len(omega.shape)  == 3, 'Data shape is not 3D, investigate' 
+  assert data[pres_var_name].max() < 1100, 'Data is in Pa and not hPa, please update.' 
+
   
   print('... Integrate first direction')
   
   for t in range(len(data['time'])): 
-    for p in range(len(data['pfull']) -1): 
+    for p in range(len(data[pres_var_name]) -1): 
       psi_lat[t,p,0]=0
       psi_lat_rev[t,p,0]=0
 
       for l in range(len( data['lat']) -1):
-        w_val = np.array([omega[t,p,l],omega[t,p+1,l],omega[t,p,l+1],omega[t,p+1,l+1]])
 
-        if np.ma.count(w_val) == 4:
-          dlat=lat_rad[l] - lat_rad[l+1]
-          psi_lat[t,p,l+1] = psi_lat[t,p,l] + dlat * fn_const_dlat * np.cos(lat_rad[l+1]) * 0.25*(np.nansum(w_val))
-        else:  
-          print('Address missing values if required')
-          pdb.set_trace()
+        w_val_data = np.array([ omega[t,p,l],   omega[t,p+1,l],
+                                omega[t,p,l+1], omega[t,p+1,l+1]])
+        mask_val   = np.array([ omega.mask[t,p,l], omega.mask[t,p+1,l],
+                                omega.mask[t,p,l+1], omega.mask[t,p+1,l+1] ])
+        w_val = np.ma.masked_array(w_val_data, mask_val)
+
+        dlat=lat_rad[l] - lat_rad[l+1]
+        psi_lat[t,p,l+1] = psi_lat[t,p,l] + dlat * fn_const_dlat * np.cos(lat_rad[l+1]) * 0.25*(np.nansum(w_val))
 
       for l in range(len(data['lat'])-1,0,-1 ): 
-        w_val_rev = np.array([omega[t,p,l],omega[t,p+1,l-1],omega[t,p,l-1],omega[t,p+1,l]])
-        if np.ma.count(w_val_rev) == 4:
-          dlat=lat_rad[l] - lat_rad[l-1]   #positive
-          psi_lat_rev[t,p,l-1] = psi_lat_rev[t,p,l]+ dlat*fn_const_dlat*np.cos(lat_rad[l-1])* 0.25*(np.nansum(w_val_rev))
-        else:
-          print('Address missing values if required')
-          pdb.set_trace()
+        w_val_rev_data = np.array([omega[t,p,l],  omega[t,p+1,l-1],
+                                   omega[t,p,l-1],omega[t,p+1,l]])
+
+        mask_val = np.array([omega.mask[t,p,l],  omega.mask[t,p+1,l-1],
+                             omega.mask[t,p,l-1],omega.mask[t,p+1,l]])
+
+        w_val_rev = np.ma.masked_array(w_val_rev_data, mask_val)
+
+
+        
+        dlat=lat_rad[l] - lat_rad[l-1]   #positive
+        psi_lat_rev[t,p,l-1] = psi_lat_rev[t,p,l]+ dlat*fn_const_dlat*np.cos(lat_rad[l-1])* 0.25*(np.nansum(w_val_rev))
     
   #integrate  bottom to top and then top to bottom [v] 
   print('... Integrate second direction')  
@@ -65,26 +74,33 @@ def cal_stream_fn(data):
       psi_p[t,0,l] = 0
       psi_p_rev[t,0,l] = 0
       
-      for p in range(len(data['pfull'])-1):
-        v_val=np.array([vcomp[t,p,l],vcomp[t,p,l+1],vcomp[t,p+1,l],vcomp[t,p+1,l+1]])
-        if np.ma.count(v_val) == 4:
-          dp = (data['pfull'][p]-data['pfull'][p+1])*100   #in Pa and negative
-          psi_p[t,p+1,l]=psi_p[t,p,l]+dp * fn_const_dp * 0.25*(np.nansum(v_val))
-        else: 
-          print('Address missing values if required')
-          pdb.set_trace()  
+      for p in range(len(data[pres_var_name])-1):
+
+        v_val_data=np.array([vcomp[t,p,l],  vcomp[t,p,l+1],
+                             vcomp[t,p+1,l],vcomp[t,p+1,l+1]])
+
+        mask_val=np.array([vcomp.mask[t,p,l],  vcomp.mask[t,p,l+1],
+                           vcomp.mask[t,p+1,l],vcomp.mask[t,p+1,l+1]])
+
+        v_val = np.ma.masked_array(v_val_data, mask_val)
+
+        dp = (data[pres_var_name][p]-data[pres_var_name][p+1])*100   #in Pa and negative
+        psi_p[t,p+1,l]=psi_p[t,p,l]+dp * fn_const_dp * 0.25*(np.nansum(v_val))
   
-      for p in range(len(data['pfull'])-1,0,-1 ):
-        v_val_rev=np.array([vcomp[t,p-1,l],vcomp[t,p-1,l+1],vcomp[t,p,l+1],vcomp[t,p,l]])
-        if np.ma.count(v_val_rev) == 4:
-          dp=(data['pfull'][p] - data['pfull'][p-1])*100   #in Pa and positive
-          psi_p_rev[t,p-1,l] = psi_p_rev[t,p,l] + dp*fn_const_dp* 0.25*(np.nansum(v_val_rev))
-        else:  
-          print('Address missing values if required')
-          pdb.set_trace()
-    
+      for p in range(len(data[pres_var_name])-1,0,-1 ):
+        v_val_rev_data=np.array([vcomp[t,p-1,l],vcomp[t,p-1,l+1],
+                                 vcomp[t,p,l+1],vcomp[t,p,l]])
+        mask_val=np.array([vcomp.mask[t,p-1,l],vcomp.mask[t,p-1,l+1],
+                           vcomp.mask[t,p,l+1],vcomp.mask[t,p,l]])
+        v_val_rev = np.ma.masked_array(v_val_rev_data, mask_val)
+
+        dp=(data[pres_var_name][p] - data[pres_var_name][p-1])*100   #in Pa and positive
+        
+        psi_p_rev[t,p-1,l] = psi_p_rev[t,p,l] + dp*fn_const_dp* 0.25*(np.nansum(v_val_rev))
+
   print('  Average the stream functions')       
-  psi_final = np.zeros([4,len(data['time']),len(data['pfull']),len(data['lat'])])
+  psi_final = np.zeros([4,len(data['time']),len(data[pres_var_name]),len(data['lat'])])
+  psi_final[...] = np.nan
   psi_final[0,:,:,:] = psi_lat
   psi_final[1,:,:,:] = psi_p
   psi_final[2,:,:,:] = psi_p_rev
@@ -92,6 +108,5 @@ def cal_stream_fn(data):
   
   #take the mean over the four stream funstions
   psi = np.mean(psi_final,axis=0)
-  
 
   return psi
