b8b9787f9e47de99196a0b75a049cd2f5e30f50c
Penelope Maher <p.maher@exeter.ac.uk>
2021-03-16 11:36:46 +0000

    This code is not performing well (the magnitude of the integration is wrong) compared to the StreamFunction_trapz.py file. This code is no longer in use but it kept for legacy)

--- StreamFunction.py
+++ StreamFunction.py
@@ -11,15 +11,23 @@ __author__ = 'Penelope Maher'
 #   [omega] = (-g/(2 pi a^2 cos (lat))) (partial Phi/ partial lat) 
 #   Psi units -> Pa m (s2/m) (m/s) = Pa m/s = kg/(ms2) (m/s) = kg /s 
 
+'''Please note this code is no longer in use. It has been replaced by the 
+   file StreamFunction_trapz. Evaluation of the two methods reveals that this
+   method is not performing as expected.
+'''
+
 def cal_stream_fn(data, pres_var_name):
   '''Calculates the mass stream function. 
-     Data is a dictionary containing masked zonal-mean arrays of omega and vcomp
-     The dims are named: time, lat, and the input pres_var_name (hPa).'''
+     Data is a dictionary containing zonal-mean arrays of omega and vcomp
+     The dims are named: time, lat, and the input pres_var_name (hPa).
+     Method: Apply the left and right Riemann Sum to approximate the area
+     (i.e. the trapezoidal rule).'''
   
-  grav=9.80665 #m/s
-  rad_earth=6378100.0 #m
+  grav=9.81 #m/s
+  rad_earth=6.371e6 #m
   lat_rad=np.radians(data['lat'])
   fn_const_dlat = (-2*(np.pi)*(rad_earth*rad_earth))/grav  
+  psi_scale_factor = 1e10
 
   #integrate right to left [omega] and then integrate left to right [omega]
   
@@ -29,9 +37,15 @@ def cal_stream_fn(data, pres_var_name):
   psi_lat_rev = np.zeros((len(data['time']),len(data[pres_var_name]),len(data['lat'])))
   omega = data['omega']
   vcomp = data['vcomp']
-  assert isinstance(omega,np.ma.core.MaskedArray)  == True , 'Data is not masked' 
   assert len(omega.shape)  == 3, 'Data shape is not 3D, investigate' 
-  assert data[pres_var_name].max() < 1100, 'Data is in Pa and not hPa, please update.' 
+
+
+  if data[pres_var_name].max() > 1100:
+    #data in Pa
+    pres_factor = 1.
+  else:
+    #data in Pa
+    pres_factor = 100.
 
   
   print('... Integrate first direction')
@@ -43,29 +57,31 @@ def cal_stream_fn(data, pres_var_name):
 
       for l in range(len( data['lat']) -1):
 
-        w_val_data = np.array([ omega[t,p,l],   omega[t,p+1,l],
-                                omega[t,p,l+1], omega[t,p+1,l+1]])
+        w_val = np.array([ omega[t,p,l],   omega[t,p+1,l],
+                           omega[t,p,l+1], omega[t,p+1,l+1]])
         mask_val   = np.array([ omega.mask[t,p,l], omega.mask[t,p+1,l],
                                 omega.mask[t,p,l+1], omega.mask[t,p+1,l+1] ])
-        w_val = np.ma.masked_array(w_val_data, mask_val)
+        w_val = np.ma.masked_array(w_val, mask_val)
 
         dlat=lat_rad[l] - lat_rad[l+1]
-        psi_lat[t,p,l+1] = psi_lat[t,p,l] + dlat * fn_const_dlat * np.cos(lat_rad[l+1]) * 0.25*(np.nansum(w_val))
+        #w_val_mean = 0.25*(np.nansum(w_val)
+        w_val_mean = np.mean(w_val)
+
+        psi_lat[t,p,l+1] = (psi_lat[t,p,l] + dlat * fn_const_dlat * np.cos(lat_rad[l+1]) * w_val_mean ) /psi_scale_factor
+        num_missing = np.where(mask_val == True)[0]
 
       for l in range(len(data['lat'])-1,0,-1 ): 
-        w_val_rev_data = np.array([omega[t,p,l],  omega[t,p+1,l-1],
-                                   omega[t,p,l-1],omega[t,p+1,l]])
+        w_val_rev = np.array([omega[t,p,l],  omega[t,p+1,l-1],
+                              omega[t,p,l-1],omega[t,p+1,l]])
 
         mask_val = np.array([omega.mask[t,p,l],  omega.mask[t,p+1,l-1],
                              omega.mask[t,p,l-1],omega.mask[t,p+1,l]])
+        w_val_rev = np.ma.masked_array(w_val_rev, mask_val)
 
-        w_val_rev = np.ma.masked_array(w_val_rev_data, mask_val)
-
-
-        
         dlat=lat_rad[l] - lat_rad[l-1]   #positive
-        psi_lat_rev[t,p,l-1] = psi_lat_rev[t,p,l]+ dlat*fn_const_dlat*np.cos(lat_rad[l-1])* 0.25*(np.nansum(w_val_rev))
-    
+        w_val_rev_mean = np.mean(w_val_rev)#0.25*(np.nansum(w_val_rev))
+        psi_lat_rev[t,p,l-1] = (psi_lat_rev[t,p,l]+ dlat*fn_const_dlat*np.cos(lat_rad[l-1])* w_val_rev_mean)/psi_scale_factor
+
   #integrate  bottom to top and then top to bottom [v] 
   print('... Integrate second direction')  
   for t in range(len(data['time'])): 
@@ -76,31 +92,31 @@ def cal_stream_fn(data, pres_var_name):
       
       for p in range(len(data[pres_var_name])-1):
 
-        v_val_data=np.array([vcomp[t,p,l],  vcomp[t,p,l+1],
-                             vcomp[t,p+1,l],vcomp[t,p+1,l+1]])
+        v_val=np.array([vcomp[t,p,l],  vcomp[t,p,l+1],
+                        vcomp[t,p+1,l],vcomp[t,p+1,l+1]])
 
         mask_val=np.array([vcomp.mask[t,p,l],  vcomp.mask[t,p,l+1],
                            vcomp.mask[t,p+1,l],vcomp.mask[t,p+1,l+1]])
 
-        v_val = np.ma.masked_array(v_val_data, mask_val)
+        v_val = np.ma.masked_array(v_val, mask_val)
 
-        dp = (data[pres_var_name][p]-data[pres_var_name][p+1])*100   #in Pa and negative
-        psi_p[t,p+1,l]=psi_p[t,p,l]+dp * fn_const_dp * 0.25*(np.nansum(v_val))
+        dp = (data[pres_var_name][p]-data[pres_var_name][p+1])*pres_factor    #in Pa and negative
+        v_val_mean = np.mean(v_val) #0.25*(np.nansum(v_val))
+        psi_p[t,p+1,l]=(psi_p[t,p,l]+dp * fn_const_dp * v_val_mean) /psi_scale_factor
   
       for p in range(len(data[pres_var_name])-1,0,-1 ):
-        v_val_rev_data=np.array([vcomp[t,p-1,l],vcomp[t,p-1,l+1],
-                                 vcomp[t,p,l+1],vcomp[t,p,l]])
+        v_val_rev=np.array([vcomp[t,p-1,l],vcomp[t,p-1,l+1],
+                            vcomp[t,p,l+1],vcomp[t,p,l]])
         mask_val=np.array([vcomp.mask[t,p-1,l],vcomp.mask[t,p-1,l+1],
                            vcomp.mask[t,p,l+1],vcomp.mask[t,p,l]])
-        v_val_rev = np.ma.masked_array(v_val_rev_data, mask_val)
+        v_val_rev = np.ma.masked_array(v_val_rev, mask_val)
 
-        dp=(data[pres_var_name][p] - data[pres_var_name][p-1])*100   #in Pa and positive
-        
-        psi_p_rev[t,p-1,l] = psi_p_rev[t,p,l] + dp*fn_const_dp* 0.25*(np.nansum(v_val_rev))
+        dp=(data[pres_var_name][p] - data[pres_var_name][p-1])*pres_factor   #in Pa and positive
+        v_val_rev_mean = np.mean(v_val_rev)#0.25*(np.nansum(v_val_rev))
+        psi_p_rev[t,p-1,l] = (psi_p_rev[t,p,l] + dp*fn_const_dp* v_val_rev_mean)/psi_scale_factor
 
   print('  Average the stream functions')       
   psi_final = np.zeros([4,len(data['time']),len(data[pres_var_name]),len(data['lat'])])
-  psi_final[...] = np.nan
   psi_final[0,:,:,:] = psi_lat
   psi_final[1,:,:,:] = psi_p
   psi_final[2,:,:,:] = psi_p_rev
@@ -109,4 +125,53 @@ def cal_stream_fn(data, pres_var_name):
   #take the mean over the four stream funstions
   psi = np.mean(psi_final,axis=0)
 
+  make_plot=False
+  if make_plot:
+        plot_stream_function(psi_p, psi_p_rev,
+                             psi_lat, psi_lat_rev, 
+                             psi, data['lat'], data[pres_var_name], omega, vcomp, pres_factor)
   return psi
+
+
+
+def plot_stream_function(psi_pres_frwd, psi_pres_bkwd,
+                         psi_lat_frwd, psi_lat_bkwd, 
+                         psi, lat, pres, omega, vwind, pres_factor):
+
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+
+    bounds=np.arange(-10,10,1)
+    cmap=plt.get_cmap('RdBu_r')
+    names = ['psi_pres_frwd','psi_pres_bkwd','psi_lat_frwd','psi_lat_bkwd',
+             'psi', 'omega', 'vwind']
+    data_list = [psi_pres_frwd,psi_pres_bkwd,psi_lat_frwd,psi_lat_bkwd,
+                 psi]#, omega, vwind]
+
+    for count, data in enumerate(data_list):
+        if count ==5:
+            bounds=np.arange(-0.1,0.11,.01)
+            label_format = '%.2f'
+        elif count ==6:
+            bounds=np.arange(-2.5,2.6,0.5)
+            label_format = '%.2f'
+        else:
+            bounds=np.arange(-10,10,1)
+            label_format = '%d'
+
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+        fig = plt.figure(figsize=(8, 8))
+        ax = fig.add_axes([0.15, 0.15, 0.8, 0.8])
+        data_plot = np.mean(data, axis=0)
+        filled_map = ax.pcolormesh(lat, (pres/pres_factor)/100., data_plot,
+                                   cmap=cmap,norm=norm, shading='nearest')
+        ax.set_ylim(1000,100)
+        ax_cb=fig.add_axes([0.15, 0.05, 0.8, 0.03]) #[xloc, yloc, width, height]
+        cbar = mpl.colorbar.ColorbarBase(ax_cb, cmap=cmap,norm=norm, ticks=bounds,
+                                         orientation='horizontal',format=label_format,
+                                         extend='both')
+        filename='testing_psi_{0}_old_code.eps'.format(names[count])
+        plt.savefig(filename)
+        plt.show()
+
